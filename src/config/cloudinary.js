fa1eae25367a3902a668e9ab34b655fe054e9e50
uttkarshnjr10 <uttkarshkumar687@gmail.com>
2026-01-05 14:26:41 +0530

    fix the dot env config in cloudinary.js

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,4 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+// Load environment
+dotenv.config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
